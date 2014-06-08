6d5df4ec1d62bb56b0636891f92fe97154a42f0a
stefan <stefan@beigel>
2014-06-08 19:52:41 +0200

    SB - remoived mocked time from sleenium tests and added it to webapptest

--- IOTaskScheduler/IOTaskSchedulerSystemTest/src/test/java/seleniumTest/webDriver/AbstractRequest.java
+++ IOTaskScheduler/IOTaskSchedulerSystemTest/src/test/java/seleniumTest/webDriver/AbstractRequest.java
@@ -2,13 +2,9 @@ package seleniumTest.webDriver;
 
 import static seleniumTest.webDriver.URL.url;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 public abstract class AbstractRequest {
 	protected WebDriver driver;
@@ -20,19 +16,6 @@ public abstract class AbstractRequest {
 		initializeWebDriver();
 		driver.get(url);
 
-		Resource timeshiftScriptResource = new ClassPathResource("timeshift.js");
-		Resource mocktimeScriptResource = new ClassPathResource("mocktime.js");
-
-		
-		if (driver instanceof JavascriptExecutor) {
-			//create string
-			String script = "";
-			script += IOUtils.toString(timeshiftScriptResource.getInputStream(), "UTF-8");
-			script += IOUtils.toString(mocktimeScriptResource.getInputStream(), "UTF-8");
-			//execute
-			((JavascriptExecutor) driver).executeScript(script);
-			
-		}
 	}
 
 	@After
